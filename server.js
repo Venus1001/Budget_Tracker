8bac3d50a1f0bc894799e2b055b7bf3f5d2c7a7c
venus <autumn.qiu@icloud.come>
2020-12-21 21:15:08 -0500

    Update

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://Venus:fifimongo$@cluster0
 
 app.use(require("./routes/api.js"));
 
+app.get("/", function(req,res){
+  res.json(path.join(__dirname,"public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
